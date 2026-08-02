270209d6d231e1b136fa75910295961c8d729b3a
impressivecoder1 <rohanrahman251@gmail.com>
2026-08-03 01:29:05 +0600

    d

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import app from "./app";
+import config from "./config";
 
+
+if(config.NODE_ENV !== 'production'){
 app.listen(5000, ()=>{
     console.log('server is running on 5000');
 })
+}
+
+export default app
