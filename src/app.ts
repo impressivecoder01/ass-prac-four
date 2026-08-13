42ff148eda354330b888c4b1b7bcb72b787128de
impressivecoder1 <rohanrahman251@gmail.com>
2026-08-14 02:28:07 +0600

    adding middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Application } from "express"
 import prisma from "./lib/prisma"
 import { json } from "node:stream/consumers"
+import { notFound } from "./middleware/notFound"
 
 const app:Application = express()
 
@@ -9,5 +10,7 @@ app.use(express.json())
 app.get("/", async(req, res)=> {
     res.send("server is done")
 })
+
+app.use(notFound)
 
 export default app
---
+++ src/middleware/notFound.ts
@@ -0,0 +1,12 @@
+import type { NextFunction,Request,Response } from "express";
+
+export const notFound = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    res.status(404).json({
+        success: false,
+        message: `Not found ${req.originalUrl}`
+    })
+}
