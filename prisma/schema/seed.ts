a4d44d456fc1fe7829ce3ab4e8fda208a15e77b2
impressivecoder1 <rohanrahman251@gmail.com>
2026-08-16 02:04:57 +0600

    make data

--- prisma/schema/seed.ts
+++ prisma/schema/seed.ts
@@ -49,7 +49,82 @@ async function main() {
         }),
 
     ])
-    
+    console.log('create ');
+
+    const gearsToCreate = [
+    {
+        brand: "yoyo",
+        model: "latest2026",
+        dailyRate: 1200,
+        // location: "dhaka"
+    },
+    {
+        brand: "yoyo2",
+        model: "latest2023",
+        dailyRate: 1200,
+        // location: "dhaka"
+    },
+    {
+        brand: "samsung",
+        model: "galaxy-s26",
+        dailyRate: 1500,
+        // location: "dhaka"
+    },
+    {
+        brand: "apple",
+        model: "iphone-17",
+        dailyRate: 2000,
+        // location: "dhaka"
+    },
+    {
+        brand: "sony",
+        model: "alpha-a7iv",
+        dailyRate: 2500,
+        // location: "dhaka"
+    },
+    {
+        brand: "canon",
+        model: "eos-r6",
+        dailyRate: 2200,
+        // location: "dhaka"
+    },
+    {
+        brand: "nikon",
+        model: "z6-iii",
+        dailyRate: 2300,
+        // location: "dhaka"
+    },
+    {
+        brand: "dji",
+        model: "mini-4-pro",
+        dailyRate: 1800,
+        // location: "dhaka"
+    },
+    {
+        brand: "gopro",
+        model: "hero13-black",
+        dailyRate: 1400,
+        // location: "dhaka"
+    },
+    {
+        brand: "lenovo",
+        model: "thinkpad-x1",
+        dailyRate: 1600,
+        // location: "dhaka"
+    },
+    {
+        brand: "asus",
+        model: "rog-strix-g16",
+        dailyRate: 1900,
+        // location: "dhaka"
+    },
+    {
+        brand: "logitech",
+        model: "mx-master-4",
+        dailyRate: 500,
+        // location: "dhaka"
+    }
+];
 }
 
 main().then(process.exit(0))
